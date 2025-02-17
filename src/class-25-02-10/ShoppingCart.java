6b58d4c410c44c36096bcc2a5a86ed5e107f9866
Josué Medino <josuemedino5@gmail.com>
2025-02-17 00:22:53 -0300

    feat: Modelagem da aula 25-02-10

---
+++ src/class-25-02-10/ShoppingCart.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    private int customerID;
+    private ArrayList<Product> productList;
+
+    public void addProduct() {}
+    public void removeProduct() {}
+    public String getContents() {}
+    public int getCustomer() {}
+    public int getItemCount() {}
+    public double getTotalPrice() {}
+}
